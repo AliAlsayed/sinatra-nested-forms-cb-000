6e8da18f252342cb4fd4a3600937ee474c23d377
Ali Alsayed <a_alsayed87@hotmail.com>
2019-01-20 07:18:56 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,8 +1,14 @@
 class Ship
   attr_accessor :name, :type, :booty
+
+  @@all = []
   def initialize(params)
     @name = params[:name]
     @type = params[:type]
     @booty = params[:booty]
+    @@ all << self
+  end
+
+  def self.clear
   end
 end
